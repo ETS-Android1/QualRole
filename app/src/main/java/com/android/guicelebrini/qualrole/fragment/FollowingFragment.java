097238e0c1b3c534a0f2e3ca5a46419ce7cbf118
GuiCelebrini <celebrinig@gmail.com>
2021-09-29 21:44:34 -0300

    Created fragments to show questions and following users

---
+++ app/src/main/java/com/android/guicelebrini/qualrole/fragment/FollowingFragment.java
@@ -0,0 +1,33 @@
+package com.android.guicelebrini.qualrole.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.guicelebrini.qualrole.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FollowingFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FollowingFragment extends Fragment {
+
+    public FollowingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_following, container, false);
+
+        return view;
+    }
+}
